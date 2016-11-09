8b10db9b3d9ee78bd355196792ce140f02994208
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-11-09 15:44:02 +0100

    Profiler files removed.

--- src/main/java/de/huberlin/wbi/cfjava/eval/DefaultProfiler.java
+++
@@ -1,9 +0,0 @@
-package de.huberlin.wbi.cfjava.eval;
-
-public class DefaultProfiler implements Profiler {
-
-	@Override
-	public void reportTime( String tag, Long startTime, Long durationMs ) {
-		// do nothing
-	}
-}
--- src/main/java/de/huberlin/wbi/cfjava/eval/Profiler.java
+++
@@ -1,5 +0,0 @@
-package de.huberlin.wbi.cfjava.eval;
-
-public interface Profiler {
-	public void reportTime( String tag, Long startTime, Long timeMs );	
-}
